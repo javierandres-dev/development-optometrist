a32f582ff1add1d5903102ea708f4edaf2c6dbcf
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-04-07 17:54:03 -0500

    feat: database separate

--- scripts/script.js
+++ scripts/script.js
@@ -1,86 +1,31 @@
-const d = document,
-  alphabet = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ],
-  animals = [
-    "Foca",
-    "Gallo",
-    "Gato",
-    "Lobo",
-    "Loro",
-    "Mono",
-    "Oso",
-    "Pato",
-    "Rana",
-    "Sapo",
-    "Toro",
-    "Vaca",
-  ],
-  countries = [
-    "Argentina",
-    "Bolivia",
-    "Brasil",
-    "Chile",
-    "Colombia",
-    "Ecuador",
-    "Guyana",
-    "Paraguay",
-    "Perú",
-    "Surinam",
-    "Uruguay",
-    "Venezuela",
-  ],
-  numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-d.addEventListener("DOMContentLoaded", (e) => {
-  show("form");
+'use strict';
+import * as data from './data.js';
+const d = document;
+d.addEventListener('DOMContentLoaded', (e) => {
+  show('form');
 });
-show = (id) => {
+const show = (id) => {
   const $form = d.getElementById(id),
     $btnStart = $form.start,
     $btnEnd = $form.end,
-    $show = d.getElementById("show"),
-    $messages = d.getElementById("messages"),
-    $div = d.createElement("div");
+    $display = d.getElementById('display'),
+    $messages = d.getElementById('messages'),
+    $div = d.createElement('div');
   let showMessage,
     showContent,
     inputs = 0;
-  d.addEventListener("change", (e) => {
+  d.addEventListener('change', (e) => {
     if (e.target) inputs += 1;
     if (inputs === 5) $btnStart.disabled = false;
     if (inputs > 5) {
-      $messages.innerText = "Seleccione solo una opción por parámetro.";
+      $messages.innerText = 'Seleccione solo una opción por parámetro.';
       inputs = 0;
       $btnStart.disabled = true;
       $btnEnd.disabled = true;
       $form.reset();
     }
   });
-  d.addEventListener("submit", (e) => {
+  d.addEventListener('submit', (e) => {
     if (e.target === $form) {
       e.preventDefault();
       $btnEnd.disabled = false;
@@ -93,8 +38,8 @@ show = (id) => {
       $div.style.fontSize = `${$size}rem`;
       $div.style.fontWeight = `${$weight}`;
       $div.classList.add(`${$move}`);
-      if ($move !== "motionless") {
-        $div.style["animationDuration"] = `${$speed}s`;
+      if ($move !== 'motionless') {
+        $div.style['animationDuration'] = `${$speed}s`;
       }
       let starting = $speed - 1;
       showMessage = setInterval(() => {
@@ -102,37 +47,37 @@ show = (id) => {
       }, 1000);
       showContent = setInterval(() => {
         clearInterval(showMessage);
-        $messages.innerText = "";
+        $messages.innerText = '';
         $div.innerText = getElements($content);
-        $show.appendChild($div);
+        $display.appendChild($div);
       }, $speed * 1000);
     }
   });
-  d.addEventListener("click", (e) => {
+  d.addEventListener('click', (e) => {
     if (e.target === $form.end) {
       clearInterval(showContent);
       inputs = 0;
-      $show.removeChild($div);
+      $display.removeChild($div);
       $form.reset();
       $btnStart.disabled = true;
       $btnEnd.disabled = true;
     }
   });
 };
-getElements = (content) => {
+const getElements = (content) => {
   let arr;
-  if (content === "alphabet") {
-    arr = alphabet;
+  if (content === 'alphabet') {
+    arr = data.alphabet;
   }
-  if (content === "animals") {
-    arr = animals;
+  if (content === 'animals') {
+    arr = data.animals;
   }
-  if (content === "countries") {
-    arr = countries;
+  if (content === 'countries') {
+    arr = data.countries;
   }
-  if (content === "numbers") {
-    arr = numbers;
+  if (content === 'numbers') {
+    arr = data.numbers;
   }
-  idx = Math.floor(Math.random() * arr.length);
+  const idx = Math.floor(Math.random() * arr.length);
   return arr[idx];
 };
