4002245668a1ea2bf531eeb6a1d51ca0a38acf9d
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-12-04 17:44:49 -0500

    v2

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,34 @@
-'use strict';
-import * as data from './data.js';
+"use strict";
+import * as data from "./data.js";
 
 const d = document,
-  $display = d.getElementById('display'),
-  $alert = d.getElementById('alert'),
-  $form = d.getElementById('myForm'),
-  $btnStart = d.getElementById('startBtn'),
-  $btnEnd = d.getElementById('endBtn'),
-  $div = d.createElement('div'),
+  $display = d.getElementById("display"),
+  $alert = d.getElementById("alert"),
+  $form = d.getElementById("myForm"),
+  $btnStart = d.getElementById("startBtn"),
+  $btnEnd = d.getElementById("endBtn"),
+  $div = d.createElement("div"),
   $bgColor = $form.bgColor,
   $bgOpacity = $form.bgOpacity,
   $bgImage = $form.bgImage,
   $contentColor = $form.contentColor,
-  $contentOpacity = $form.contentOpacity,
-  $content = $form.content,
-  $size = $form.size,
-  //$weight = $form.weight,
-  $move = $form.move,
-  $speed = $form.speed;
+  $contentOpacity = $form.contentOpacity;
 
 let myObject = {
-  content: '',
-  size: '',
-  //weight: '',
-  move: '',
-  speed: '',
+  content: "",
+  move: "",
+  speed: "",
 };
 
 let interval = undefined;
 
-d.addEventListener('DOMContentLoaded', (e) => {
+d.addEventListener("DOMContentLoaded", (e) => {
   eventListeners();
-  showAlert('warning', 'Seleccione campos obligatorios *');
+  showAlert("warning", "Seleccione campos obligatorios *");
 });
 
 const eventListeners = () => {
-  $form.addEventListener('change', (e) => {
+  $form.addEventListener("change", (e) => {
     if (e.target === $bgColor) {
       $display.style.backgroundColor = $bgColor.value;
     }
@@ -51,77 +44,70 @@ const eventListeners = () => {
         $contentOpacity.value
       );
     }
-    if (e.target.name === 'bgImage') {
-      let url = undefined;
-      if (
-        $bgImage.value === 'baseball' ||
-        $bgImage.value === 'basketball' ||
-        $bgImage.value === 'soccer' ||
-        $bgImage.value === 'tennis' ||
-        $bgImage.value === 'voleyball'
-      ) {
-        url = `../images/${$bgImage.value}.jpeg`;
-      }
+    if (e.target.name === "bgImage") {
+      const url = `../images/${$bgImage.value}.jpeg`;
       $display.setAttribute(
-        'style',
-        'background-image: url(' +
+        "style",
+        "background-image: url(" +
           url +
-          ');background-repeat: no-repeat;background-position: 50% 50%;background-size: 100% auto;'
+          ");background-repeat: no-repeat;background-position: 50% 50%;background-size: 100% auto;"
       );
     }
-    if (e.target.name === 'content') {
-      myObject.content = $content.value;
+    if (e.target.name === "content") myObject.content = e.target.value;
+    if (e.target.name === "move") {
+      if (e.target.value === "random") {
+        const moves = [
+          "x-ltr",
+          "x-rtl",
+          "y-ttb",
+          "y-btt",
+          "d-ttb",
+          "d-btt",
+          "ad-ttb",
+          "ad-btt",
+        ];
+        const idx = Math.floor(Math.random() * moves.length);
+        myObject.move = moves[idx];
+      } else {
+        myObject.move = e.target.value;
+      }
     }
-    if (e.target.name === 'size') {
-      myObject.size = $size.value;
-    }
-    //if (e.target.name === 'weight') {
-    //myObject.weight = $weight.value;
-    //}
-    if (e.target.name === 'move') {
-      myObject.move = $move.value;
-    }
-    if (e.target.name === 'speed') {
-      myObject.speed = $speed.value;
-    }
-    enableSubmit();
+    if (e.target.name === "speed") myObject.speed = e.target.value;
+    enableStart();
   });
 
-  $btnStart.addEventListener('click', () => {
+  $btnStart.addEventListener("click", () => {
     showContent();
     showAlert(
-      'success',
+      "success",
       'Para cambiar parámetros o detener la prueba presione el botón de "Finalizar"'
     );
     $btnStart.disabled = true;
     $btnEnd.disabled = false;
   });
 
-  $btnEnd.addEventListener('click', () => {
-    console.log('btnEnd click... ');
+  $btnEnd.addEventListener("click", () => {
     clearInterval(interval);
     $display.removeChild($div);
     myObject = {
-      content: '',
-      size: '',
-      //weight: '',
-      move: '',
-      speed: '',
+      content: "",
+      move: "",
+      speed: "",
     };
     $display.innerHTML = null;
     $display.style.backgroundImage = null;
-    $display.style.backgroundColor = '#000000';
-    $display.style.color = '#FFFFFF';
-    showAlert('warning', 'Seleccione campos obligatorios');
+    $display.style.backgroundColor = "#000000";
+    $display.style.color = "#FFFFFF";
+    showAlert("warning", "Seleccione campos obligatorios");
     $form.reset();
     $btnEnd.disabled = true;
   });
 };
 
-const enableSubmit = () => {
+const enableStart = () => {
   if (isValid()) {
     $btnStart.disabled = false;
-    showAlert('success', 'Presione el botón "Iniciar"');
+    showAlert("success", 'Presione el botón "Iniciar"');
   }
 };
 
@@ -139,13 +125,10 @@ const isValid = () => {
 };
 
 const showContent = () => {
-  //const { content, size, weight, move, speed } = myObject;
-  const { content, size, move, speed } = myObject;
-  $div.style.fontSize = `${size}rem`;
-  //$div.style.fontWeight = `${weight}`;
+  const { content, move, speed } = myObject;
   $div.classList.add(`${move}`);
-  if (move !== 'motionless') {
-    $div.style['animationDuration'] = `${speed}s`;
+  if (move !== "motionless") {
+    $div.style["animationDuration"] = `${speed}s`;
   }
   interval = setInterval(() => {
     $div.innerText = getElements(content);
@@ -153,31 +136,26 @@ const showContent = () => {
   }, speed * 1000);
 };
 
-const showAlert = (type = '', message = '') => {
+const showAlert = (type = "", message = "") => {
   $alert.className = `alert ${type}`;
   $alert.textContent = message;
 };
 
 const getElements = (content) => {
-  let arr = undefined;
-  if (content === 'alphabet') {
-    arr = data.alphabet;
-  }
-  if (content === 'animals') {
-    arr = data.animals;
-  }
-  if (content === 'countries') {
-    arr = data.countries;
-  }
-  if (content === 'numbers') {
-    arr = data.numbers;
-  }
+  let arr = null;
+  if (content === "numbers") arr = data.numbers;
+  if (content === "lowercase") arr = data.lowercase;
+  if (content === "uppercase") arr = data.uppercase;
+  if (content === "countries") arr = data.countries;
+  if (content === "fruits") arr = data.fruits;
+  if (content === "sports") arr = data.sports;
+  if (content === "words") arr = data.words;
   const idx = Math.floor(Math.random() * arr.length);
   return arr[idx];
 };
 
 const convertHexToRGBA = (hexCode, opacity) => {
-  const hex = hexCode.replace('#', '');
+  const hex = hexCode.replace("#", "");
 
   if (hex.length === 3) {
     hex = `${hex[0]}${hex[0]}${hex[1]}${hex[1]}${hex[2]}${hex[2]}`;
